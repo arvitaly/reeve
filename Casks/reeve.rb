bb661b704121fb9f344c3152af033f0fb0fadf88
clover <clover@Vitalys-Mac-mini.local>
2026-05-02 23:05:28 +0400

    Release v0.2.7

--- Casks/reeve.rb
+++ Casks/reeve.rb
@@ -1,8 +1,8 @@
 cask "reeve" do
-  version "0.2.6"
+  version "0.2.7"
   # Run `make release VERSION=#{version}` (or `make notarize`) and paste the
   # sha256 printed at the end. Update both version and sha256 on every release.
-  sha256 "942f28779b237565c1a6a1673b573b05479a71551ba1d2c0e37988f2a297daf8"
+  sha256 "90dbf5070bfca3f8435b5b055e0caccc046c4a5f867f15146510179d1f841988"
 
   url "https://github.com/arvitaly/reeve/releases/download/v#{version}/Reeve-#{version}.zip"
   name "Reeve"
